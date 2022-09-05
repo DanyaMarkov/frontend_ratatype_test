57d297cd0d24458051c42e2d93f951f1e558c426
roqzych <roqzych@gmail.com>
2022-09-05 11:08:14 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import "./App.css";
-// import Counter from "./components/Counter";
-
-// import Layout from "./components/Layout/Layout/Layout";
-
-import { Route, Routes } from "react-router-dom";
-import MainMenu from "./components/MainMenu/MainMenu";
-import Game from "./components/Game/Game";
-import Map from "./components/Map/Map";
+import Body from "./components/Body/Body";
 
 function App() {
     return (
         <div className="App">
-            {/* <MainMenu /> */}
-            <Routes>
-                <Route path="" element={<MainMenu />} />
-                <Route path="game/*" element={<Game />} />
-                <Route path="map/*" element={<Map />} />
-                {/* <Route path="profile/*" element={<ToDo />} /> */}
-            </Routes>
+            <Body />
         </div>
     );
 }
