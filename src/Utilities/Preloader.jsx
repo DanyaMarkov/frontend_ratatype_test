875695ca6a5d7aa876db0ed036642df5f2f7e932
roqzych <roqzych@gmail.com>
2022-09-18 13:44:30 +0300

    Генерация текста по средству API запросов

---
+++ src/Utilities/Preloader.jsx
@@ -0,0 +1,20 @@
+function Preloader() {
+    return (
+        <div className="preloader-wrapper big active">
+            <div className="spinner-layer spinner-blue-only">
+                <div className="circle-clipper left">
+                    <div className="circle"></div>
+                </div>
+                <div className="gap-patch">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-clipper right">
+                    <div className="circle"></div>
+                </div>
+            </div>
+            {/* 11111111111111111 */}
+        </div>
+    );
+}
+
+export { Preloader };
